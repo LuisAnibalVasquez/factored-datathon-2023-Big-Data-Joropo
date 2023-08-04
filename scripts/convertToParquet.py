fa6833c739cb27fdb90eb8f61607add0b8bfdbd6
osmar-dev <oaba.dev@gmail.com>
2023-08-04 18:45:03 -0400

    comments on script

--- scripts/convertToParquet.py
+++ scripts/convertToParquet.py
@@ -4,30 +4,36 @@ import json
 import pandas as pd
 import time
 
+#Search all the folders in amazon_reviews_folder
 allFolders = os.listdir('amazon_reviews_folder')
 allFolders.remove('amazon_reviews')
 
+#Empty list
 listJsonObjects = []
+
+#Counters
 cont = 1
 cont_parts = 1
-# df1 = pl.DataFrame()
 start_time = time.time()
 for folder in allFolders:
-
+    #Search of all the folders fo the reviews
     files = os.listdir('amazon_reviews_folder/'+folder)
     for file in files:
         if ".gz" not in file:
             continue
-
+        
+        #Searching for the file that is needed
         currentWorkingDirectory = os.getcwd()
         folderPath = os.path.join(os.path.join(currentWorkingDirectory,'amazon_reviews_folder'),folder)
         filePath = os.path.join(folderPath,file)
+
+        #Filling the list
         with gzip.open(filePath, 'rb') as fileMetadata:
             for row in fileMetadata:
                 listJsonObjects.append(json.loads(row.decode().strip()))
-                # df2 = pl.from_dict(json.loads(row.decode().strip()))
-                # df1 = pl.concat([df1, df2], rechunk=True)
         cont += 1
+
+        #The conversion into parquet
         if cont%100==0:
             print('comienza la conversion')
             df=pd.json_normalize(listJsonObjects)
@@ -35,8 +41,6 @@ for folder in allFolders:
             print('Termina la conversion')
             listJsonObjects = []
             cont_parts += 1
-        print(cont)
 
 end_time = time.time()
 print(end_time-start_time)
-print("Funciono")
