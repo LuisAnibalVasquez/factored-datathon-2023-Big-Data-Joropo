f736ee033f38cf6ba5f456f08397f42e3e75ed23
Luis Vásquez <luisanibalvasquez@gmail.com>
2023-08-05 14:39:07 -0400

    API documentation updated

--- app/main.py
+++ app/main.py
@@ -8,6 +8,36 @@ description = """
 You will be able to:
 * Predict the sentiment on a given review
 * Get product recommdation for user
+
+Sentiment analysis:
+This method makes use of the Pattern library to generate the sentiment of a reviews, 
+at the same time it generates a confidence measurement that supports said sentiment.
+
+Book recommendation:
+This model is trained based on 330K products belonging to the Books category of the total existing categories
+A neural network was developed using the TensorFlow Recommender System library
+
+Instructions for using the API
+
+Click on the method you want to use:
+* /sentiment/{review}
+* /recommend/{reviewerid}
+
+Click the "Try it out" button
+
+Type the Review or Reviewer ID in the corresponding field.
+
+Click on the "Execute" button.
+
+Scroll to the bottom to the "Response" section specifically to the "Response body" section
+
+In the case of sentiment analysis, you will obtain two decimal numbers which correspond to sentiment analysis and confidense.
+The first number corresponded to the sentiment of the review, if it is positive the sentiment will be classified as Affirmative, 
+if it is a negative number the sentiment will be classified as Negative.
+The second number returned corresponds to the level of confidence with which the method supports the predicted sentiment.
+
+In the case of the method for recommendations, it will return a list with 10 titles of the recommended free books.
+
 """
 
 app = FastAPI(
